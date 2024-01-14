979b412753b2e3107fb11638caae518e12f434b9
Brian <brianababu63@gmail.com>
2024-01-14 22:14:43 +0300

    refactored the main.kt file

--- app/src/main/java/Main.kt
+++ app/src/main/java/Main.kt
@@ -1,26 +1,25 @@
-package com.example.abstractvsinterface
-
 //import com.example.abstractvsinterface.accounts.CheckingAccount
-import com.example.abstractvsinterface.accounts.SalaryAccount
+//import com.example.abstractvsinterface.accounts.SalaryAccount
 import com.example.abstractvsinterface.brokers.StockBroker
 
-//import com.example.abstractvsinterface.accounts.SavingsAccount
+import com.example.abstractvsinterface.accounts.SavingsAccount
 
 fun main() {
-    val salary = SalaryAccount(100000.0)
-    //val savings = SavingsAccount(1000.0)
+    //val salary = SalaryAccount(100000.0)
+    val savings = SavingsAccount(1000.0)
     //val checking = CheckingAccount(500.0)
 
-    salary.deposit(10000.0)
-    salary.withdrawViaTeller(2000.0)
-    salary.withdrawViaAtm(2000.0)
+    //salary.deposit(10000.0)
+    //salary.withdrawViaTeller(2000.0)
+    //salary.withdrawViaAtm(2000.0)
 
-//    savings.deposit(500.0)
+      savings.deposit(500.0)
+      savings.withdraw(200.0)
 //    checking.withdraw(200.0)
 //
-//    println("${savings.accountType()} has balance: ${savings.getBalance()}")
+    println("${savings.accountType()} has balance: ${savings.getBalance()}")
 //    println("${checking.accountType()} has balance: ${checking.getBalance()}")
-    println("${salary.accountType()} has balance: ${salary.getBalance()}")
+   // println("${salary.accountType()} has balance: ${salary.getBalance()}")
 
 
     val stockBroker = StockBroker()
